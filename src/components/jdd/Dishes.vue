acd265b9083fe4f8ee94ede8d9390b68fb725624
gxp <gxp@163.com>
2019-10-18 20:39:26 +0800

    更新点餐页面

---
+++ src/components/jdd/Dishes.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <div class="bg">欢迎光临，简单点餐厅！</div>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return {
+
+        }
+    }
+}
+</script>
+<style scoped>
+    .bg{
+        width: 100%;
+        height: 100%;
+        background: #ff0000;
+    }
+    
+</style>
